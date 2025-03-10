51c0ace193ead53c2af441f6d3e87fdaef56df7a
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-03-10 17:53:35 +0800

    add Overload and Override

---
+++ lesson2/lesson2-class-constructor/src/duotai.java
@@ -0,0 +1,34 @@
+//test Override and Overload
+public class duotai {
+    class Person{
+        public String run(){
+            return "Person-run";
+        }
+    }
+    class Student extends Person{
+        @Override
+        public String run() {
+            return "Student-run";
+        }
+    }
+    class Chongzai{
+        int add(int a,int b){
+            return a+b;
+        }
+        double add(double a,double b){
+            return a+b;
+        }
+        int add(int a,int b,int c){
+            return a+b+c;
+        }
+    }
+    public static void main(String[] args) {
+        duotai a=new duotai();
+        Person ap=a.new Person();
+        System.out.println(ap.run());
+        Student as=a.new Student();
+        System.out.println(as.run());
+        Chongzai ac=a.new Chongzai();
+        System.out.println(ac.add(5,6)+" "+ac.add(5.5,7.8)+" "+ac.add(99,44,66));
+    }
+}
