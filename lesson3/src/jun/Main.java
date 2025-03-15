6fd53b0e3e08d2784e0ed2cd4b451c343b16fe18
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-03-15 23:31:48 +0800

    try package

---
+++ lesson3/src/jun/Main.java
@@ -0,0 +1,9 @@
+//尝试用了别的包的类
+package jun;
+import ming.Person;
+public class Main {
+    public static void main(String[] args) {
+        Person a=new Person("jun",16);
+        System.out.println(a.getName()+a.getAge());
+    }
+}
