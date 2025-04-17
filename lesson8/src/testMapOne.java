88f0309e6d1017228210c00bfe0ea4a5db0086d3
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-04-17 20:04:01 +0800

    add some change

---
+++ lesson8/src/testMapOne.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+public class testMapOne {
+    public static void main(String[] args) {
+        String key1="a";
+        Map<String,Integer> map=new HashMap<>();
+        map.put(key1,123);
+        String key2=new String("a");
+        map.get(key2);
+        System.out.println(key1==key2);
+        System.out.println(key1.equals(key2));
+    }
+}
