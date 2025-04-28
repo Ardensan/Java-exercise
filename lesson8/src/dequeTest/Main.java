d5a593fc462b698ed727f9743036027bef13cc13
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-04-28 16:48:18 +0800

    change some thing

--- lesson8/src/dequeTest/Main.java
+++ lesson8/src/dequeTest/Main.java
@@ -1,7 +1,14 @@
 package dequeTest;
-
+import java.util.Deque;
+import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
-        
+        Deque<String> deque=new LinkedList<>();
+        deque.offerLast("a");
+        deque.offerLast("b");
+        deque.offerFirst("c");
+        System.out.println(deque);
+        System.out.println(deque.pollLast());
+        System.out.println(deque.pollFirst());
     }
 }
