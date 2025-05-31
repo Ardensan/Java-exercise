3eff093813257c89bd73f211465402f4759834cb
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-05-31 23:11:19 +0800

    add new

---
+++ lesson12/src/testThree/two.java
@@ -0,0 +1,15 @@
+package testThree;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class two {
+    public static void main(String[] args) {
+        Pattern p=Pattern.compile("(\\d+?)(0*)");
+        Matcher m=p.matcher("123000");
+        if(m.matches()){
+            System.out.println(m.group(1));
+            System.out.println("第二组是"+m.group(2));
+        }
+    }
+}
