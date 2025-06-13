15f60f2f49db805de2691fe113e0793de4ae925a
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-06-13 17:53:07 +0800

    add new

---
+++ lesson14/src/testOne/one.java
@@ -0,0 +1,9 @@
+package testOne;
+
+public class one {
+    public static void main(String[] args) {
+        Thread t=new Thread();
+        System.out.println("线程已启动");
+        t.start();
+    }
+}
