91e7f0af00e51e7a47829fec26f1a9140e7b8a9f
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-06-19 21:28:55 +0800

    add new folder

---
+++ lesson13/src/testTwo/one.java
@@ -0,0 +1,9 @@
+package testTwo;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("0x"+Integer.toHexString("java".hashCode()));
+        System.out.println("0x"+Integer.toHexString("hello,bill".hashCode()));
+        System.out.println("0x"+Integer.toHexString("sdjiwudisad".hashCode()));
+    }
+}
---
+++ lesson13/src/testTwo/two.java
@@ -0,0 +1,8 @@
+package testTwo;
+
+public class two {
+    public static void main(String[] args) {
+        System.out.println("0x"+Integer.toHexString("AaAaAa".hashCode()));
+        System.out.println("0x"+Integer.toHexString("BBAaBB".hashCode()));
+    }
+}
