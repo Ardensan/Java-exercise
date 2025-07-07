28854bc2748c645fa9b99e70a691cbad706426ea
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-07-07 23:00:36 +0800

    add new

---
+++ lesson14/src/testFour/two.java
@@ -0,0 +1,10 @@
+package testFour;
+
+public class two {
+    public static void main(String[] args) {
+        String name="bill";
+        System.out.println(name.repeat(5));
+        System.out.println("\n" + "=".repeat(50) + "\n");
+        //.repeat(50)将当前的字符串重复 50次 并返回一个新的字符串。
+    }
+}
