c7e7b82cd35e2f153c3bf20743b333e809f89fd0
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-07-26 23:07:24 +0800

    add new

---
+++ lesson14/src/testClaude/two.java
@@ -0,0 +1,13 @@
+package testClaude;
+
+import java.util.Scanner;
+
+public class two {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("test");
+        String pw=scanner.nextLine();
+        scanner.close();
+        System.out.println(pw);
+    }
+}
