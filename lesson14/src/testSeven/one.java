16e04ed84e16533710cde7254a4ea3f2e8d8b446
Ardensan <26789743+Ardensan@users.noreply.github.com>
2025-07-27 23:58:27 +0800

    add new folder

---
+++ lesson14/src/testSeven/one.java
@@ -0,0 +1,27 @@
+package testSeven;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Task implements Runnable{
+    private final String name;
+    public Task(String name){this.name=name;}
+    @Override
+    public void run(){
+        System.out.println("start task"+ name);
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+        }
+        System.out.println("end task"+name);
+    }
+}
+public class one {
+    public static void main(String[] args) {
+        ExecutorService es= Executors.newFixedThreadPool(4);
+        for(int i=0;i<6;i++){
+            es.submit(new Task(" "+i));
+        }
+        es.shutdown();
+    }
+}
